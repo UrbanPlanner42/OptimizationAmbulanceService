2c240b7654837d839057ceec10640fa27cf00dc6
Alexis Soto-Colorado <asc589@nyu.edu>
2017-07-26 18:08:56 -0400

    Function to predict Erlag B NUA

---
+++ nua_schedule.R
@@ -0,0 +1,89 @@
+library(lubridate)
+library(ggplot2)
+library(dplyr, warn.conflicts = TRUE)
+
+#Load Data
+#incident <- read.csv("~/Desktop/NewOrleans-master-80ea90a6f4b0dbc93132a0d886ba6398c8992632/Data/2016_11_Incidents.csv")
+incident <- read.csv("~/Desktop/NewOrleans-master-80ea90a6f4b0dbc93132a0d886ba6398c8992632/Data/all_Incidents.csv")
+
+# #Earl B Function 
+erlb <- function(nsrv, nerl, log=FALSE) {
+  # Returns Erlang-B blockage for nsrv servers and nerl erlangs of traffic.
+  # If log=TRUE, then logarithm of the blockage is returned.  Works for all
+  # nsrv>=0 and all nerl>=0.  Both nsrv and nerl can be non-integers.
+
+  lnerlb<-
+    dgamma(nerl, shape=nsrv+1, scale=1, log=TRUE) -
+    pgamma(nerl, shape=nsrv+1, scale=1, log.p=TRUE, lower.tail=FALSE) ;
+  if (log) lnerlb else exp(lnerlb)
+}
+
+#Function to USE
+elf <- function(n, a) { 
+  b <- 1 ; i<- 0 
+  while((i <- i+1)<=n) b <- b*a/(i+b*a) 
+  b 
+} 
+
+#Testing different functions to get Erlag B before I write the function
+erlb(5, 4.05)
+elf(5, 4.05)
+
+
+#X is the Data Frame for 911 Data
+#Y is the month
+#C numbers of ambulances
+#Z Begin of the Shift
+#E End of the Shift 
+
+#City Wide Function 
+
+nua_schedule <- function(x, c, y, d, z, e) {
+    #Converting to Time Format Time in R
+    x$IncidentDate_Time <- ymd_hms(x$IncidentDate) #Get R compatible date
+    x$Year <- year(x$IncidentDate_Time)
+    x$Month <- month(x$IncidentDate_Time) #Day of the month
+    x$DoW <- weekdays(x$IncidentDate_Time) #Get day of week
+    x$IncidentDate_Date <- date(x$IncidentDate_Time) #Strip date from full format
+    x$HoD <- hour(x$IncidentDate_Time)
+    #Cleaning Dispatch Time and Closed Time
+    x$BeginCase_Time <- ymd_hms(x$DispatchedTime)
+    x$CloseCase_Time <- ymd_hms(x$ClosedTime)
+    x$How_Long_Case <- (x$CloseCase_Time - x$BeginCase_Time)/60 #Get closed cases in Minute 
+    
+    #Summarize by Year
+    gp1 <- group_by(x, Year, Month, DoW, HoD, How_Long_Case)
+    gp1 <- gp1 %>%
+           select(Year, Month, DoW, HoD, How_Long_Case) %>%
+           group_by(Year, Month, DoW, HoD) %>%
+           summarise_all(funs(Count_Cases = n(),
+                              Average_CaseTime = mean(How_Long_Case, na.rm = TRUE)))
+    
+    
+    #Summarize by Hour, Days, and Month
+    gp2 <- group_by(gp1, Month, DoW, HoD, Count_Cases, Average_CaseTime)
+    
+    gp2 <- gp2 %>%
+      group_by(Month, DoW, HoD) %>%
+      summarise_each_(vars = c("Count_Cases", "Average_CaseTime"), 
+                      funs = funs(Median="median"))
+    
+    gp2$ValueA_Erlag <- (gp2$Average_CaseTime_Median * gp2$Count_Cases_Median)/60 #Get the A in Erlag B formula
+    gp2$Numbers_Ambulance <- c
+    
+    gp2 <- gp2[gp2$Month == y, ] #Month
+    gp2 <- gp2[gp2$DoW == d, ] #Day
+    gp2 <- gp2[gp2$HoD >= z & gp2$HoD <= e, ]  #Time of the shift
+    
+    #ErlagB <- mapply(elf, gp2$Numbers_Ambulance, gp2$ValueA_Erlag) #Run the function for ErlagB 
+    ErlagB <- mapply(erlb, gp2$Numbers_Ambulance, gp2$ValueA_Erlag) #Run the function for ErlagB 
+    gp2$NUA_Probability <- as.data.frame(ErlagB)
+    
+    return(as.data.frame(gp2))
+    }
+
+
+#Save the Ouput of the Function 
+Schedule_For_Tuesday <- nua_schedule(incident, 25, 11, "Wednesday", 0, 4)
+
+Schedule_For_Tuesday$NUA_Probability
